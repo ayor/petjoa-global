d0ffc3690c1889b15616073a2f7ed6d67af0f4bb
ayor <39896394+ayor@users.noreply.github.com>
2021-02-05 09:14:34 +0100

    fixed the header Navlink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,7 +14,7 @@ const Header = props => (
                         <p className="display-4  text-center text-capitalize  text-light font-weight-bold"><span className="text-success">Petjoa</span><span className="">Global</span> Investment Limited</p>
                     </div>
                     <div className={headerClass.Banner}>
-                        <NavLink className="btn btn-success d-block w-100" >Get A Loan <i className="ml-3 fa fa-money"></i></NavLink>
+                        <NavLink to="/" className="btn btn-success d-block w-100" >Get A Loan <i className="ml-3 fa fa-money"></i></NavLink>
                     </div>
                 </div>
             </div>
